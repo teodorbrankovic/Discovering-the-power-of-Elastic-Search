cca3d941f0cc5d2fd265db986b1569c7021203b1
Teodor Brankovic <teobrankovic1999@gmail.com>
2024-10-15 23:11:03 +0200

    Dab

---
+++ DAB/DAB/DABApp.swift
@@ -0,0 +1,17 @@
+//
+//  DABApp.swift
+//  DAB
+//
+//  Created by Teodor Brankovic on 20.04.24.
+//
+
+import SwiftUI
+
+@main
+struct DABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
